906ec644384d4c4791778bef100b2b4544b64549
hayden-110 <99691272+hayden-110@users.noreply.github.com>
2024-07-17 17:31:50 +0100

    Without indoor

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -22,6 +22,22 @@ export default {
 					950: '#000000'
 				},
 				'bumble': '#fbd40f',
+				'ikea': '#007afc',
+				'cake': {
+					DEFAULT: '#FF73A9',
+					50: '#FFFFFF',
+					100: '#FFFFFF',
+					200: '#FFEDF4',
+					300: '#FFC5DB',
+					400: '#FF9CC2',
+					500: '#FF73A9',
+					600: '#FF3B87',
+					700: '#FF0364',
+					800: '#CA004E',
+					900: '#920038',
+					950: '#76002D'
+				},
+
 			},
 			fontFamily: {
 				'pandore': ['"Pandore"', ...defaultTheme.fontFamily.sans],
@@ -34,6 +50,16 @@ export default {
 				'max-h': 'max-height',
 				'spacing': 'margin, padding',
 				'position': 'top, right, bottom, left'
+			},
+			fontSize: {
+				'h1': ['clamp(2rem, 2.75vw + 1rem, 4rem)', '1.2'],
+				'h2': ['clamp(1.8rem, 1rem + 2.25vw, 3.1748rem)', '1.2'],
+				'h3': ['clamp(1.6rem, 1rem + 1.75vw, 2.5198rem)', '1.2'],
+				'h4': ['clamp(1.4rem, 1rem + 1.25vw, 2rem)', '1.2'],
+				'h5': ['clamp(1.2rem, 1rem + .75vw, 1.5874rem)', '1.2'],
+				'h6': ['clamp(1rem, 1rem + .25vw, 1.25rem)', '1.2'],
+				'lead': ['clamp(1.125rem, 1rem + .5vw, 1.35rem)', '1.2'],
+				'small': ['clamp(.8rem, .8rem + .125vw, 1rem)', '1.2'],
 			}
 		},
 	},
