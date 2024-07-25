8ce7085ab83814f12a26462bd8823d7fdab226e4
hayden-110 <99691272+hayden-110@users.noreply.github.com>
2024-07-25 20:43:25 +0100

    squoosh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
@@ -11,4 +11,7 @@ export default defineConfig({
   adapter: cloudflare({
     imageService: 'cloudflare'
   }),
+  image: {
+    service: squooshImageService(),
+  },
 });
