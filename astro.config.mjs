e09a947b69f0dc8b0097eb996dcbac990d26caf6
hayden-110 <99691272+hayden-110@users.noreply.github.com>
2025-02-25 18:49:59 +0000

    final commit for duplicate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig, } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: cloudflare({
-    imageService: 'compile'
-  }),
 });
